2a91e304ae114f372009f0413aba976505a09cb0
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-12-06 06:39:39 +0100

    Add more parse functions, add typing function for sql.js results

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { QueryExecResult } from "sql.js";
+import { QueryExecResult, SqlValue } from "sql.js";
 
 export const sqliteInfoToIntermediate = (_result: unknown) => {
   return "TODO";
@@ -8,7 +8,7 @@ interface Column {
   name: string;
   type: number | string | Uint8Array;
   nullable: boolean;
-  default: number | string | Uint8Array | null;
+  default: SqlValue;
 }
 
 interface Table {
@@ -22,39 +22,41 @@ interface Index {
   primaryKey: boolean;
 }
 
-type FN_ACTION = "CASCADE" | "TODO WRITE THEM ALL";
+type FN_ACTION = "CASCADE" | "RESTRICT" | "TODO WRITE THEM ALL";
 
 interface ForeignKey {
   from: Table;
   fromColumns: Column[];
-  to: Table;
-  toColumns: Column[];
+  // not the complete types as they aren't available at the time of FN upsert
+  to: string;
+  toColumns: string[];
   onUpdate: FN_ACTION;
   onDelete: FN_ACTION;
 }
+const typeResult = <T>(result: QueryExecResult): T[] => {
+  return result.values.map((row) => {
+    const obj: { [key: string]: SqlValue } = {};
+    result.columns.forEach((col, index) => {
+      obj[col] = row[index];
+    });
+    return obj as T;
+  });
+};
 
-/*
-cid	name	type	notnull	dflt_value	pk
-0	personnummer	TEXT	0	NULL	1
-1	funktionshindrad	INTEGER	0	NULL	0
-*/
 export const tableFromResult = (tableName: string, result: QueryExecResult): { table: Table, primaryKey: Index } => {
-  const resultColumns = result.columns;
-  const columns: Column[] = result.values.map((row) => {
-    const nameIndex = resultColumns.indexOf("name");
-    const typeIndex = resultColumns.indexOf("type");
-    const notNullIndex = resultColumns.indexOf("type");
-    const defaultIndex = resultColumns.indexOf("dflt_value");
+  const typedResult = typeResult<{ cid: number, name: string, type: string, notnull: number, dflt_value: SqlValue, pk: number }>(result);
+  const columns: (Column & { pk: boolean })[] = typedResult.map((row) => {
     return {
-      name: row[nameIndex] as string,
-      type: row[typeIndex] as string,
-      nullable: !(row[notNullIndex] === 1),
-      default: row[defaultIndex]
+      name: row.name,
+      type: row.type,
+      nullable: !(row.notnull === 1),
+      default: row.dflt_value,
+      pk: row.pk === 1
     };
   });
 
   const primaryKey: Index = {
-    columns: columns.filter((_column, index) => result.values[index][resultColumns.indexOf("pk")] === 1),
+    columns: columns.filter((col) => col.pk),
     unique: true,
     primaryKey: true,
   };
@@ -65,22 +67,73 @@ export const tableFromResult = (tableName: string, result: QueryExecResult): { t
   };
 };
 
+export const foreignKeysFromResult = (table: Table, result: QueryExecResult): ForeignKey[] => {
+  const typedResult = typeResult<{ id: number, seq: number, table: string, from: string, to: string, on_update: string, on_delete: string, match: string }>(result);
+  const partialForeignKeys: { [id: number]: { toTable: string, onUpdate: FN_ACTION, onDelete: FN_ACTION, columns: { from: string, to: string }[] } } = {};
+  for (const row of typedResult) {
+    const id = row.id;
+    if (!partialForeignKeys[id]) {
+      partialForeignKeys[id] = {
+        toTable: row.table,
+        onUpdate: row.on_update as FN_ACTION,
+        onDelete: row.on_delete as FN_ACTION,
+        columns: []
+      };
+    }
+
+    partialForeignKeys[id].columns.push({
+      from: row.from,
+      to: row.to
+    });
+  }
+
+  const foreignKeys: ForeignKey[] = Object.values(partialForeignKeys).map((fk) => {
+    return {
+      from: table,
+      fromColumns: fk.columns.map((col) => table.columns.find((c) => c.name === col.from)!),
+      to: fk.toTable,
+      toColumns: fk.columns.map((col) => col.to),
+      onUpdate: fk.onUpdate,
+      onDelete: fk.onDelete
+    };
+  });
+
+  return foreignKeys;
+};
+
+// PRAGMA index_list(table)
+/*
+seq	name	unique	origin	partial
+0	sqlite_autoindex_Student_1	1	pk	0
+*/
+// PRAGMA index_info(index)
+/*
+seqno	cid	name
+0	0	personnummer
+*/
+
+export const indexesFromResult = (table: Table, indexListResult: QueryExecResult, indexInfoResult: { [indexName: string]: QueryExecResult }): Index[] => {
+  const typedIndexListResult = typeResult<{ seq: number, name: string, unique: number, origin: string, partial: number }>(indexListResult);
+  const typedInfoResults: { [indexName: string]: { seqno: number, cid: number, name: string }[] } = Object.entries(indexInfoResult).map(([indexName, result]) => {
+    return {
+      [indexName]: typeResult<{ seqno: number, cid: number, name: string }>(result)
+    };
+  }).reduce((acc, val) => {
+    return { ...acc, ...val };
+  }, {});
+  // TODO: Implement
+};
+
 export class SQLiteLayout {
   // tables = name: Table[]
   // indexes = tableName: Index[]
   // foreignKeys =  fromtable: ForeignKey[]
-  private tables: Table[];
-  private indexes: Index[];
-  private foreignKeys: ForeignKey[];
-
-  constructor() {
-    this.tables = [];
-    this.indexes = [];
-    this.foreignKeys = [];
-  }
+  private tables: { [name: string]: Table } = {};
+  private indexes: { [tableName: string]: Index } = {};
+  private foreignKeys: ForeignKey[] = [];
 
   public addTable(table: Table) {
-    this.tables.push(table);
+    this.tables[table.name] = table;
   }
 
   
