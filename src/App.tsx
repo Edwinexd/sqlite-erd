3fe01d4d9f86639bff4fad05d5371d6b556db80e
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-12-23 00:03:31 +0100

    Remove accidental console logs

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,6 @@ function App() {
     });
 
     const dot = layout.getDot();
-    console.log(dot);
     dotToSvg(dot).then((svg) => {
       setErdSVG(svg);
     }).catch((error) => {
@@ -118,7 +117,6 @@ function App() {
     }
 
     const finalSVG = colorErdSVG(erdSVG, isDarkMode());
-    console.log(finalSVG);
 
     setErdImage(`data:image/svg+xml;base64,${Buffer.from(finalSVG, "utf-8").toString("base64")}`);
   }, [erdSVG, isDarkMode]);
